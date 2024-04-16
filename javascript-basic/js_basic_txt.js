62c1e496b58d0e95dce0b8dcff32081f7b72bbdc
dataiti <nguyendat16111210@gmail.com>
2024-04-16 23:29:25 +0700

    update js_basic_txt file

--- javascript-basic/js_basic_txt.js
+++ javascript-basic/js_basic_txt.js
@@ -1,12 +1,8 @@
 // 1. Khời tạo 1 array có 2000 phần tử có các key như sau (id, name, address, gender, phoneNumber, description, source) sao cho
 // name & source không trùng nhau
-// - Viết 1 function (searchItemBynameOrSource) để tìm kiếm 1 item dựa trên source hoặc name trong array trên (có thể làm nhiều cách)
-// - Tạo 1 form input để show lên màn hình.
-// - Cho bối cảnh như sau: giả sử người dùng sẽ dùng hàm này nhiều lần hoặc rất nhiều lần cùng 1 thời điểm, làm thế nào để tối ưu
-// hiệu suất của function trên, viết lại function có tên "improveSearchItemBynameOrSource" nếu có.
 let arrs = [];
 
-for (let index = 0; index <= 5; index++) {
+for (let index = 0; index < 10; index++) {
   arrs.push({
     id: index,
     name: `name-${index}`,
@@ -18,10 +14,11 @@ for (let index = 0; index <= 5; index++) {
   });
 }
 
+// - Tạo 1 form input để show lên màn hình.
 const input = document.querySelector(".searchText");
 const container = document.querySelector(".result-container");
 
-const renderList = ({ arrsRender = arrs }) => {
+const renderList = (arrsRender) => {
   container.innerHTML = "";
 
   arrsRender.forEach((arr) => {
@@ -31,45 +28,48 @@ const renderList = ({ arrsRender = arrs }) => {
   });
 };
 
-const searchItemBynameOrSource = ({ searchText = "" }) => {
-  let filterArrs = arrs;
-
-  if (searchText) {
-    const search = searchText.toString().toLowerCase();
-
-    filterArrs = arrs.filter(
-      ({ name, source }) =>
-        name.toString().toLowerCase() === search ||
-        source.toString().toLowerCase() === search
-    );
+// - Viết 1 function (searchItemBynameOrSource) để tìm kiếm 1 item dựa trên source hoặc name trong array trên (có thể làm nhiều cách)
+const searchItemBynameOrSource = (searchText) => {
+  if (!searchText) {
+    renderList(arrs);
+    return;
   }
 
-  renderList({ arrsRender: filterArrs });
+  const search = searchText.toLowerCase().trim();
+
+  const filterArrs = arrs.filter(
+    ({ name, source }) =>
+      name.toLowerCase().includes(search) ||
+      source.toLowerCase().includes(search)
+  );
+
+  renderList(filterArrs);
 };
 
-// searchItemBynameOrSource({ searchText: "source-1" });
-
-const debounce = (fn, delay) => {
-  let timerId = null;
-  return (...args) => {
-    clearTimeout(timerId);
-    timerId = setTimeout(() => fn(...args), delay);
+// - Cho bối cảnh như sau: giả sử người dùng sẽ dùng hàm này nhiều lần hoặc rất nhiều lần cùng 1 thời điểm, làm thế nào để tối ưu
+// hiệu suất của function trên, viết lại function có tên "improveSearchItemBynameOrSource" nếu có.
+function debounce(func, delay) {
+  let timeoutId;
+  return function (...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
   };
-};
+}
 
-const improveSearchItemBynameOrSource = ({ searchText }) => {
-  debounce(searchItemBynameOrSource({ searchText }), 5000);
-};
+const improveSearchItemBynameOrSource = debounce(searchItemBynameOrSource, 700);
 
 input.addEventListener("input", (e) => {
-  improveSearchItemBynameOrSource({ searchText: e.target.value });
+  improveSearchItemBynameOrSource(e.target.value);
 });
 
-renderList({ arrsRender: arrs });
-
+renderList(arrs);
 // 2. Tạo 1 form input để nhập số (là số ngày đi làm tiếp theo, không tính t7, CN), hãy viết function trả về kết quả ngày đó là thứ mấy trong tuần.
 // ex: nhập 3 sẽ trả về "thứ 5"
-const DAY_IN_WEEK = [
+
+// constant
+const DAYS_IN_WEEK = [
   { id: 0, value: "Thứ hai" },
   { id: 1, value: "Thứ ba" },
   { id: 2, value: "Thứ tư" },
@@ -79,12 +79,12 @@ const DAY_IN_WEEK = [
   { id: 6, value: "Chủ nhất" },
 ];
 
+// variable
 const inputNumber = document.querySelector(".inputNumber");
 const result = document.querySelector(".result");
 
+// function
 const getNextDay = ({ dayNumber }) => {
-  let nextDayValue;
-
   if (!dayNumber) {
     result.innerText = "Ngay khong dc bo trong";
     return;
@@ -93,17 +93,24 @@ const getNextDay = ({ dayNumber }) => {
   const currenDate = new Date();
   const nextDayIndex = (currenDate.getDay() - 1 + Number(dayNumber)) % 7;
 
-  let nextDay = DAY_IN_WEEK.find((day) => day.id === nextDayIndex);
+  let nextDay = DAYS_IN_WEEK.find((day) => day.id === nextDayIndex);
 
-  if (nextDay.id >= 5) nextDayValue = DAY_IN_WEEK[0].value;
-  else nextDayValue = nextDay.value;
+  let nextDayValue;
+  if (nextDay.id >= 5) {
+    nextDayValue = DAYS_IN_WEEK[0].value;
+  } else {
+    nextDayValue = nextDay.value;
+  }
 
   return nextDayValue;
 };
 
+// event handler
 inputNumber.addEventListener("input", (e) => {
   const nextDayValue = getNextDay({ dayNumber: e.target.value });
-  if (nextDayValue) result.innerText = nextDayValue;
+  if (nextDayValue) {
+    result.innerText = nextDayValue;
+  }
 });
 
 // 3. Viết 1 hàm tính toán số lớn bằng js, với các phép tính(+,-,*,/). Yêu cầu
@@ -113,8 +120,10 @@ inputNumber.addEventListener("input", (e) => {
 // - Có comment, giải thích
 // - Tạo 1 form UI đơn giản để show lên màn hình.
 // Có thể tham khảo https://www.calculator.net/big-number-calculator.html để tham khảo UI & kết quả tính toán
-const number1 = document.querySelector(".inputNumber1");
-const number2 = document.querySelector(".inputNumber2");
+
+// variable
+const number1Element = document.querySelector(".inputNumber1");
+const number2Element = document.querySelector(".inputNumber2");
 
 const additionBtnElement = document.querySelector(".addition-btn");
 const subtractionBtnElement = document.querySelector(".subtraction-btn");
@@ -123,32 +132,55 @@ const divisionBtnElement = document.querySelector(".division-btn");
 
 const calculateResultElement = document.querySelector(".calculate-result");
 
+// function
+const calculate = (operator) => {
+  if (!number1Element.value || !number2Element.value) {
+    calculateResultElement.innerText =
+      "number 1 hoặc number 2 khôgng được bỏ trống";
+    return;
+  }
+
+  try {
+    const number1 = Number(BigInt(number1Element.value));
+    const number2 = Number(BigInt(number2Element.value));
+
+    let result;
+    if (operator === "+") {
+      result = number1 + number2;
+    } else if (operator === "-") {
+      result = number1 - number2;
+    } else if (operator === "*") {
+      result = number1 * number2;
+    } else if (operator === "/") {
+      if (number2 === 0) {
+        result = "Không thể chia cho 0";
+      } else {
+        result = number1 / number2;
+      }
+    }
+    console.log(result);
+
+    calculateResultElement.innerText = result.toString();
+  } catch (error) {}
+};
+
+// event handler
 additionBtnElement.addEventListener("click", (e) => {
   e.preventDefault();
-  calculateResultElement.innerText =
-    Number(number1.value) + Number(number2.value);
+  calculate("+");
 });
 
 subtractionBtnElement.addEventListener("click", (e) => {
   e.preventDefault();
-
-  calculateResultElement.innerText =
-    Number(number1.value) - Number(number2.value);
+  calculate("-");
 });
 
 multiplicationBtnElement.addEventListener("click", (e) => {
   e.preventDefault();
-
-  calculateResultElement.innerText =
-    Number(number1.value) * Number(number2.value);
+  calculate("*");
 });
 
 divisionBtnElement.addEventListener("click", (e) => {
   e.preventDefault();
-  if (Number(number2.value) === 0)
-    calculateResultElement.innerText = "Không thể chia cho 0";
-  try {
-    calculateResultElement.innerText =
-      Number(number1.value) / Number(number2.value);
-  } catch (error) {}
+  calculate("/");
 });
